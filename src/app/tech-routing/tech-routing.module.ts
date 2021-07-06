bb65e6016e96b45995addeb9832a9e764a69b552
bhargawarao1999 <85075303+bhargawarao1999@users.noreply.github.com>
2021-07-06 17:43:55 +0530

    Update tech-routing.module.ts

--- src/app/tech-routing/tech-routing.module.ts
+++ src/app/tech-routing/tech-routing.module.ts
@@ -6,17 +6,12 @@ import { JavaComponent } from '../java/java.component';
 import { ProductsComponent } from '../products/products.component';
 import { AdvJavaComponent } from '../adv-java/adv-java.component';
 import { CoreJavaComponent } from '../core-java/core-java.component';
+import { HtmlCourseComponent } from '../html-course/html-course.component';
+import { CssCourseComponent } from '../css-course/css-course.component';
 
 const routes: Routes = [
-  { path: 'web', component: WebComponent ,children : [
-    { path: 'htmlcourse', component: CoreJavaComponent },
-    { path: 'csscourse', component: AdvJavaComponent },
-  ]},
-  { path: 'java', component: JavaComponent ,
-  children : [
-    { path: 'corejava', component: CoreJavaComponent },
-    { path: 'advjava', component: AdvJavaComponent },
-  ]},
+  { path: 'web', component: WebComponent},
+  { path: 'java', component: JavaComponent},
   { path: 'product', component: ProductsComponent }
 ]
 @NgModule({
